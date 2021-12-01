184c77530b3ea6fc9c5a3e57c3e7548764b03359
DUPORT Quentin <duport.quentin69@gmail.com>
2021-12-01 21:39:14 +0100

    Ajout fonctionalité de Card.cs + mise en place des effets et des conditions dans ListEnumerator.cs

--- StarRealms/Assets/Resources/Script/Card.cs
+++ StarRealms/Assets/Resources/Script/Card.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Transactions;
+using System.Xml;
+using UnityEditor.Search;
 using UnityEngine;
 
 namespace Resources.Script
 {
     public class Card : MonoBehaviour
     {
+        private static int nbCard = 0;
         public int id;
         public new string name;
         public int cost;
@@ -18,10 +22,21 @@ namespace Resources.Script
         public int baseLife;
         public bool isUsed;
         public bool needPlayer;
+        public int montantD = 0;
+        public int montantG = 0;
+        public int montantH = 0;
+        public int montantDraw = 0;
+        public int montantScrap = 0;
+        public Card mySelf = null;
 
+        public Card()
+        {
+            
+        }
         public Card(int id, string name, int cost, Faction faction, bool shipOrBase, bool isTaunt, Mesh image, Sprite icon, Dictionary<Condition,List<Effect>> actions, int baseLife,bool needPlayer, bool isUsed)
         {
-            this.id = id;
+            nbCard++;
+            this.id = nbCard;
             this.name = name;
             this.cost = cost;
             this.faction = faction;
@@ -33,74 +48,281 @@ namespace Resources.Script
             this.baseLife = baseLife;
             this.isUsed = isUsed;
             this.needPlayer = needPlayer;
+
+            foreach (List<Effect> lesEffets in Actions.Values)
+            {
+                if (lesEffets.Contains(Effect.Copy))
+                {
+                    mySelf = GameObject.Instantiate(this);
+                }
+            }
         }
-    
-        public void DoEffect()
+        public void PlaySelf()
         {
-        
+            GameManager.currentPlayer.PlayCard(this);
         }
-        
+        public void CheckCondition()
+        {
+            foreach (Condition cond in Actions.Keys)
+            {
+                switch (cond)
+                {
+                    case Condition.Nothing:
+                        DoEffect(Actions[cond]);
+                        break;
+                    case Condition.Or:
+                        /*choix du joueur*/
+                        int choix = 0;
+                        List<Effect> aFaire = new List<Effect>();
+                        aFaire.Add(Actions[cond][choix]);
+                        DoEffect(aFaire);
+                        break;
+                    case Condition.Synergie:
+                        if (CheckFactionOnBoard())
+                        {
+                            DoEffect(Actions[cond]);
+                        }
+                        break;
+                    case Condition.AutoScrap:
+                        /*Confirmation player*/
+                        DoEffect(Actions[cond]);
+                        Destroy(this);
+                        break;
+                    case Condition.ForEachSameFaction:
+                        for (int i = 0; i < CheckNbSameFactionOnBoard(); i++)
+                        {
+                            DoEffect(Actions[cond]);
+                        }
+                        break;
+                    case Condition.TwoBaseOrMore:
+                        if (CheckNbBaseOnBoard() >= 2)
+                        {
+                            DoEffect(Actions[cond]);
+                        }
+                        break;
+                }
+            }
+        }
+        public void DoEffect(List<Effect> lesEffets)
+        {
+            foreach (Effect e in lesEffets)
+            {
+                switch (e)
+                {
+                    case Effect.Copy:
+                        Copy();
+                        break;
+                    case Effect.D:
+                        AddValue(Effect.D,montantD);
+                        break;
+                    case Effect.Discard:
+                        break;
+                    case Effect.Draw:
+                        GameManager.currentPlayer.Draw(montantDraw);
+                        break;
+                    case Effect.G:
+                        AddValue(Effect.G,montantG);
+                        break;
+                    case Effect.H:
+                        AddValue(Effect.H,montantH);
+                        break;
+                    case Effect.Hinder:
+                        /*choix du joueur*/
+                        break;
+                    case Effect.Requisition:
+                        Requisition();
+                        break;
+                    case Effect.Sabotage:
+                        TargetDiscard();
+                        break;
+                    case Effect.Scrap:
+                        ScrapOrDiscard(false,montantScrap,Zone.HandAndDiscardPile,false);
+                        break;
+                    case Effect.Wormhole:
+                        GameManager.currentPlayer.cardOnTop = true;
+                        break;
+                    case Effect.BaseDestruction:
+                        DestroyTargetBase();
+                        break;
+                    case Effect.DiscardToDraw:
+                        /*choix du joueur*/
+                        int nbDiscard = 0;
+                        GameManager.currentPlayer.Draw(nbDiscard);
+                        break;
+                    case Effect.AllShipOneMoreDamage:
+                        foreach (Card card in GameManager.currentPlayer.board)
+                        {
+                            if (!card.shipOrBase && card.montantD > 0)
+                            {
+                                card.AddValue(Effect.D,1);
+                            } 
+                        }
+                        break;
+                }
+            }
+        }
         public bool CheckFactionOnBoard()
         {
-            /*foreach (Card c in GameManager.currentPlayer.board)
+            foreach (Card c in GameManager.currentPlayer.board)
             {
-                if (c.faction == faction)
+                if (c.faction == faction || c.faction == Faction.All)
                 {
                     return true;
                 }
-            }*/
+            }
             return false;
         }
-        
-        public void AddValue(String type, int value)
+        public int CheckNbSameFactionOnBoard()
         {
-            /*switch (type)
+            int nbSameFaction = 0;
+            foreach (Card c in GameManager.currentPlayer.board)
             {
-                case "Hp":
-                    currentPlayer.hp += value;
-                    break;
-                case "Power":
-                    currentPlayer.totalPower += value;
-                    break;
-                case "Money":
-                    currentPlayer.money += value;
-                    break;
-            }*/
+                if (c.faction == faction || c.faction == Faction.All)
+                {
+                    nbSameFaction++;
+                }
+            }
+            return nbSameFaction;
         }
-
-        public void Scrap(int nbMax, ScrapZone zone, bool mustScrap)
+        public void AddValue(Effect type, int value)
         {
-            
+            switch (type)
+            {
+                case Effect.H:
+                    GameManager.currentPlayer.hp += value;
+                    break;
+                case Effect.D:
+                    GameManager.currentPlayer.totalPower += value;
+                    break;
+                case Effect.G:
+                    GameManager.currentPlayer.money += value;
+                    break;
+            }
         }
-
+        public void ScrapOrDiscard(bool scrapOrDiscard, int nbMax, Zone zone, bool mustDoIt)
+        {
+            switch(zone)
+            {
+                case Zone.Hand:
+                    if (!scrapOrDiscard)
+                    {
+                        /*choix du joueur*/
+                        Card c = new Card();
+                        Destroy(c);
+                    }
+                    else
+                    {
+                        //GameManager.currentPlayer.Discard(nbMax);
+                    }
+                    break;
+                case Zone.DiscardPile:
+                    if (!scrapOrDiscard)
+                    {
+                        /*choix du joueur*/
+                        Card c = new Card();
+                        Destroy(c);
+                    }
+                    break;
+                case Zone.HandAndDiscardPile:
+                    if (!scrapOrDiscard)
+                    {
+                        /*choix du joueur*/
+                        Card c = new Card();
+                        Destroy(c);
+                    }
+                    break;
+                case Zone.Shop:
+                    if (!scrapOrDiscard)
+                    {
+                        /*choix du joueur*/
+                        Card c = new Card();
+                        Destroy(c);
+                    }
+                    break;
+            }
+        }
         public void TargetDiscard()
         {
-            
+            if (GameManager.currentPlayer == GameManager.player1)
+            {
+                GameManager.player2.toDiscard += 1;
+            } else if (GameManager.currentPlayer == GameManager.player2)
+            {
+                GameManager.player1.toDiscard += 1;
+            }
         }
-
-        public void DestroyTargetBase(Card card)
+        public void DestroyTargetBase()
         {
-            
+            /*Choix du joueur*/
+            Card card = new Card();
+            Destroy(card);
         }
-
         public void Requisition()
         {
-            
+            GameManager.currentPlayer.freeShip = true;
         }
-
-        public void CheckNbBase()
+        public int CheckNbBaseOnBoard()
         {
-            
+            int nbBase = 0;
+            foreach (Card c in GameManager.currentPlayer.board)
+            {
+                if (c.shipOrBase)
+                {
+                    nbBase++;
+                }
+            }
+            return nbBase;
         }
-
-        public void Copy(Card card)
+        public void Copy()
         {
-            
+            /*choix du joueur*/
+            Card card = new Card();
+            name = card.name;
+            cost = card.cost;
+            faction = card.faction;
+            shipOrBase = card.shipOrBase;
+            isTaunt = card.isTaunt;
+            icon = card.icon;
+            image = card.image;
+            baseLife = card.baseLife;
+            isUsed = card.isUsed;
+            needPlayer = card.needPlayer;
+
+            Condition cond = FindConditionOfEffect(Effect.Copy);
+            Actions.Remove(cond);
+            foreach (KeyValuePair<Condition, List<Effect>> k in card.Actions)
+            {
+                Actions.Add(k.Key,k.Value);
+            }
+            CheckCondition();
         }
-
-        public void ToDiscard(int maxDiscard)
+        public void UnCopy()
         {
-            
+            name = mySelf.name;
+            cost = mySelf.cost;
+            faction = mySelf.faction;
+            shipOrBase = mySelf.shipOrBase;
+            isTaunt = mySelf.isTaunt;
+            icon = mySelf.icon;
+            image = mySelf.image;
+            Actions = mySelf.Actions;
+            baseLife = mySelf.baseLife;
+            isUsed = mySelf.isUsed;
+            needPlayer = mySelf.needPlayer;
+        }
+        private Condition FindConditionOfEffect(Effect e)
+        {
+            foreach ( KeyValuePair<Condition,List<Effect>> k in Actions)
+            {
+                foreach (Effect effect in k.Value)
+                {
+                    if (effect == e)
+                    {
+                        return k.Key;
+                    }
+                }
+            }
+            return Condition.NotFound;
         }
     }
 }
--- StarRealms/Assets/Resources/Script/ListEnumerator.cs
+++ StarRealms/Assets/Resources/Script/ListEnumerator.cs
@@ -1,7 +1,7 @@
 namespace Resources.Script
 {
-    public enum Faction {a,b,c,d}
-    public enum Condition{a,b,c,d}
-    public enum Effect{a,b,c,d}
-    public enum ScrapZone{a,b,c,d}
+    public enum Faction {Rouge, Vert, Jaune, Bleu, All}
+    public enum Condition{Nothing,Or,Synergie,AutoScrap,TwoBaseOrMore,ForEachSameFaction,NotFound}
+    public enum Effect{D,Discard,Draw,G,H,Sabotage,Wormhole,Hinder,Requisition,Scrap,Copy,DiscardToDraw,AllShipOneMoreDamage,BaseDestruction}
+    public enum Zone{Hand,DiscardPile,HandAndDiscardPile,Shop}
 }
